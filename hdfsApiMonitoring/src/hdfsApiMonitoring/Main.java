734a6acc1a44170eb52895b07baf3c44d56f6926
mlanciau <mlanciau@gmail.com>
2016-07-11 20:29:39 +0200

    first commit

---
+++ hdfsApiMonitoring/src/hdfsApiMonitoring/Main.java
@@ -0,0 +1,47 @@
+package hdfsApiMonitoring;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileStatus;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+
+public class Main {
+
+	public static void main(String[] args) {
+		String postgres_host = args[0];
+		String postgres_user = args[1];
+		String postgres_password = args[2];
+		Configuration conf = new Configuration();
+		if (args.length > 3 && args[3] != " ") {
+			conf.addResource(new Path(args[3]));
+		}
+		if (args.length > 4 && args[4] != " ") {
+			conf.addResource(new Path(args[4]));
+		}
+		try {
+			Class.forName("org.postgresql.Driver");
+			Connection connection = DriverManager.getConnection("jdbc:postgresql://" + postgres_host + ":5432/test", postgres_user, postgres_password);
+			Statement statement = connection.createStatement();
+			FileSystem fs = FileSystem.get(conf);
+			FileStatus[] fsStatus = fs.listStatus(new Path("/"));
+			for(int i = 0; i < fsStatus.length; i++){
+				System.out.println(fsStatus[i].getPath().toString());
+			}
+			fs.close();
+			statement.close();
+			connection.close();
+		} catch (IllegalArgumentException | IOException e) {
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+}
